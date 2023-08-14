61678014dc7857e84546972f6b027c99e8c6ae79
Jmacosta <jmab2k@gmail.com>
2023-08-14 22:44:54 +0200

    resolve courseinfo 1.1

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,26 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-const Hello = (props) => {
+import ReactDOM from "react-dom/client";
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+const Content = (props) => {
   return (
-    <div>
-      <p>
-        Hello World {props.name} you are {props.age}
-      </p>
-    </div>
+    <p>
+      {props.part} {props.exercise}
+    </p>
   );
 };
-
+const Total = (props) => (
+  <p>
+    Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+  </p>
+);
 const App = () => {
-  const dog = "Arya";
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercise1 = 10;
+  const part2 = "Using props to pass data";
+  const exercise2 = 7;
+  const part3 = "State of a component";
+  const exercise3 = 14;
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Juanma" age="44" />
-      <Hello name="Ines" />
-      <Hello name={dog} />
+      <Header course={course} />
+      <Content part={part1} exercise={exercise1} />
+      <Content part={part2} exercise={exercise2} />
+      <Content part={part3} exercise={exercise3} />
+      <Total
+        exercise1={exercise1}
+        exercise2={exercise2}
+        exercise3={exercise3}
+      />
     </div>
   );
 };
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
