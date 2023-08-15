8d2da22500fe2bb01ec9259f890f1f8245b51c4c
Jmacosta <jmab2k@gmail.com>
2023-08-15 14:12:00 +0200

    resolve 1.3

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import React from "react";
 import ReactDOM from "react-dom/client";
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercise}
     </p>
+  );
+};
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.content1.name} exercise={props.content1.exercises} />
+      <Part part={props.content2.name} exercise={props.content2.exercises} />
+      <Part part={props.content3.name} exercise={props.content3.exercises} />
+    </div>
   );
 };
 const Total = (props) => (
@@ -17,22 +28,25 @@ const Total = (props) => (
 );
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercise1 = 10;
-  const part2 = "Using props to pass data";
-  const exercise2 = 7;
-  const part3 = "State of a component";
-  const exercise3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = { name: "State of a component", exercises: 14 };
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercise1} />
-      <Content part={part2} exercise={exercise2} />
-      <Content part={part3} exercise={exercise3} />
+      <Content content1={part1} content2={part2} content3={part3} />
+
       <Total
-        exercise1={exercise1}
-        exercise2={exercise2}
-        exercise3={exercise3}
+        exercise1={part1.exercises}
+        exercise2={part2.exercises}
+        exercise3={part3.exercises}
       />
     </div>
   );
