19218c7ad17bbc9d3e1ebfb884567c8bb4d91254
Jmacosta <jmab2k@gmail.com>
2023-08-18 19:38:25 +0200

    feat: resolve setp 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,13 +2,40 @@ import { useState } from "react";
 import Button from "./Button";
 import DisplayData from "./DisplayData";
 
+const Allvalues = ({ feedback }) => {
+  <DisplayData
+    label="ALL"
+    dataValue={feedback.good + feedback.neutral + feedback.bad}
+  />;
+};
+
+const Average = ({ feedback }) => (
+  <DisplayData
+    label="Average"
+    dataValue={
+      (feedback.good * 1 + feedback.neutral * 0 + feedback.bad * -1) /
+      (feedback.good + feedback.neutral + feedback.bad)
+    }
+  />
+);
+
+const PositivePercentage = ({ feedback }) => (
+  <DisplayData
+    label="Positive"
+    dataValue={
+      Math.floor(
+        (feedback.good * 100) /
+          (feedback.good + feedback.neutral + feedback.bad)
+      ) + " %"
+    }
+  />
+);
+
 function App() {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
-  const allValues = feedback.good + feedback.neutral + feedback.bad;
-  const average =
-    (feedback.good * 1 + feedback.neutral * 0 + feedback.bad * -1) / allValues;
-  const positivePercentage =
-    Math.floor((feedback.good * 100) / allValues) + " %";
+
+  // const positivePercentage =
+  //   Math.floor((feedback.good * 100) / allValues) + " %";
   return (
     <>
       <div>
@@ -37,9 +64,9 @@ function App() {
         <DisplayData label="Good" dataValue={feedback.good} />
         <DisplayData label="Neutral" dataValue={feedback.neutral} />
         <DisplayData label="Bad" dataValue={feedback.bad} />
-        <DisplayData label="All" dataValue={allValues} />
-        <DisplayData label="Average" dataValue={average} />
-        <DisplayData label="Positive" dataValue={positivePercentage} />
+        <Allvalues feedback={feedback} />
+        <Average feedback={feedback} />
+        <PositivePercentage feedback={feedback} />
       </div>
     </>
   );
