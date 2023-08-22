bbd36ac72d910b45815dc07ec202db0f28d50bd7
Jmacosta <jmab2k@gmail.com>
2023-08-22 12:45:33 +0200

    resolve step 1 anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const NextAnecdoted = ({ onClick }) => {
+  return <button onClick={onClick}>Next anecdote</button>;
+};
+
+function App({ anecdotes }) {
+  const [selected, setSelected] = useState(0);
+  const onClickNextAnecdotes = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {anecdotes[selected]}
+      <div style={{ marginTop: "20px" }}>
+        <NextAnecdoted onClick={onClickNextAnecdotes}></NextAnecdoted>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
