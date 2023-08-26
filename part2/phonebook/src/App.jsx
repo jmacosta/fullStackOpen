769398ae0d72404b58732fb9251a9e605f291ac9
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-08-26 21:46:09 +0200

    Feat:resolve step 3 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "999 999 999" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      phone: newPhone,
+    };
 
+    if (
+      !persons.find((person) => {
+        return person.name === personObject.name;
+      })
+    ) {
+      setPersons(persons.concat(personObject));
+    } else {
+      alert(`${personObject.name} is already added to the phoneBook`);
+    }
+    setNewName("");
+    setNewPhone("");
+  };
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input value={newName} onChange={handlePersonChange} /> Phone:{" "}
+          <input value={newPhone} onChange={handlePhoneChange} />{" "}
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => {
+          return (
+            <li key={person.name}>
+              {person.name} {person.phone}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
